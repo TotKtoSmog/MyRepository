e17aecc682615fc314e5f1090e95be66fcda7b59
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-10-09 22:29:23 +0300

    person1 First Commit

---
+++ User.cs
@@ -0,0 +1,23 @@
+using System.Xml.Serialization;
+
+namespace Parser
+{
+    [XmlRoot("user")]
+    public class User
+    {
+        [XmlElement("user_id")]
+        public long UserId { get; set; }
+        [XmlElement ("tg_name")]
+        public string TgName { get; set; }
+        public User() 
+        {
+            UserId = 0;
+            TgName = "";
+        }
+        public User(long userId, string tgName)
+        {
+            UserId = userId;
+            TgName = tgName;
+        }
+    }
+}
---
+++ XMLHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace Parser
+{
+    internal static class XMLHandler
+    {
+        public static string path = "data.xml";
+        public static User ReadXML()
+        {
+            User channelInfo = new();
+            if (System.IO.File.Exists(path))
+            {
+                XmlSerializer formatter = new(typeof(User));
+                
+                using (FileStream fs = new(path, FileMode.OpenOrCreate))
+                {
+                    channelInfo = formatter.Deserialize(fs) as User;
+                }
+
+            }
+            return channelInfo;
+        }
+        public static void WriteXML(User info)
+        {
+            User temp = new();
+
+            temp = ReadXML();
+
+            bool flag = false;
+
+            for (int i = 0; i < temp.Channels.Count; i++)
+            {
+                if (temp.Channels[i].ChatID == info.ChatID)
+                {
+                    temp.Channels[i] = info;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) temp.Channels.Add(info);
+
+            XmlSerializer serialize = new(typeof(User));
+            using (FileStream fs = new(path, FileMode.Create))
+                serialize.Serialize(fs, temp);
+            Console.WriteLine("Объект записан в XML-документ.");
+        }
+    }
+}
