a541290a62df86a7403dce6fda729c6e147c89c3
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-10-10 09:00:48 +0300

    person1 Fifth Commit

---
+++ Chat.cs
@@ -0,0 +1,25 @@
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace Parser
+{
+    [XmlRoot("chat")]
+    public class Chat
+    {
+        [XmlElement("Chat_id")]
+        public long ChatlId { get; set; }
+        [XmlElement ("Chat_title")]
+        public string ChatTitle { get; set; }
+        [XmlElement("Channel_id")]
+        public long ChannelId { get; set; }
+        [XmlElement ("Channel_Users")]
+        public List<User> Users;
+        public Channel() 
+        {
+            ChatlId = 0;
+            ChannelId = 0;
+            ChatTitle = "Empty";
+            Users = [];
+        }
+    }
+}
